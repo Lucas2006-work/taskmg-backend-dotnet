d69c8b6965c85c3861a04e2fd01b5512d72bc0b9
Lucas <lucascamposber2006@gmail.com>
2025-03-27 21:47:28 +0900

    Added hangfire service

--- TaskManagement.API/Program.cs
+++ TaskManagement.API/Program.cs
@@ -3,8 +3,20 @@ using TaskManagement.Infrastructure.Persistence;
 using TaskManagement.Infrastructure.Repositories;
 using TaskManagement.Domain.Interfaces;
 using TaskManagement.Domain.Services;
+using TaskManagement.Worker;
+
+using Hangfire;
+using Hangfire.SqlServer;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddHangfire(config =>
+    config.SetDataCompatibilityLevel(CompatibilityLevel.Version_180)
+          .UseSimpleAssemblyNameTypeSerializer()
+          .UseRecommendedSerializerSettings()
+          .UseSqlServerStorage(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddHangfireServer();
 
 builder.Services.AddDbContext<AppDbContext>(options => 
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
@@ -19,6 +31,14 @@ builder.Logging.AddConsole();
 builder.Logging.SetMinimumLevel(LogLevel.Information);
 
 var app = builder.Build();
+
+app.UseHangfireDashboard("/hangfire");
+
+RecurringJob.AddOrUpdate<TaskUpsertJob>(
+    "upsert-task-job",
+    job => job.UpsertTask(),
+    Cron.Hourly
+    );
 
 app.UseHttpsRedirection();
 
---
+++ TaskManagement.Worker/TaskUpsertJob.cs
@@ -0,0 +1,30 @@
+﻿using Hangfire;
+using TaskManagement.Domain.Entities;
+using TaskManagement.Domain.Interfaces;
+using TaskManagement.Infrastructure.Repositories;
+
+namespace TaskManagement.Worker
+{
+    public class TaskUpsertJob
+    {
+        private readonly ITaskRepository _taskRepository;
+
+        public TaskUpsertJob(ITaskRepository taskRepository)
+        {
+            _taskRepository = taskRepository;
+        }
+
+        [AutomaticRetry(Attempts = 3)]
+        public void UpsertTask()
+        {
+            var newTask = new TaskEntity
+            {
+                Title = "Automated Task",
+                Description = "This task was created by Hangfire job",
+                IsCompleted = false
+            };
+
+            _taskRepository.Add(newTask);
+        }
+    }
+}
